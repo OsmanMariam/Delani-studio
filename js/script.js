387a159ea34fada6380e1daaaca0a42808bc09f1
OsmanMariam <sufimariam8@gmail.com>
2019-09-29 23:15:10 +0300

    add about us section and welcome section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".img1").hover(function() {
+      $(".p1").toggle();
+    });
+    $(".img2").hover(function() {
+        $(".p2").toggle();
+    });
+    $(".img3").hover(function() {
+        $(".p3").toggle();
+    });
+  });
