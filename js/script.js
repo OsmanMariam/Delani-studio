84eabf22d46c34f1dfc4f183a2a02f2422543216
OsmanMariam <sufimariam8@gmail.com>
2019-09-30 08:00:39 +0300

    add contact form to the page

--- js/script.js
+++ js/script.js
@@ -8,4 +8,29 @@ $(document).ready(function() {
     $(".img3").click(function() {
         $(".p3").toggle();
     });
+
+    $(".A").hover(function() {
+        $(".pa").toggle();
+    });
+    $(".B").hover(function() {
+        $(".pb").toggle();
+    });
+    $(".C").hover(function() {
+        $(".pc").toggle();
+    });
+    $(".D").hover(function() {
+        $(".pd").toggle();
+    });
+    $(".E").hover(function() {
+        $(".pe").toggle();
+    });
+    $(".F").hover(function() {
+        $(".pf").toggle();
+    });
+    $(".G").hover(function() {
+        $(".pg").toggle();
+    });
+    $(".H").hover(function() {
+        $(".ph").toggle();
+    });
   });
